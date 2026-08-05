ecaf01ca83b185cf3f7c2c5d013cd0a71d5c3013
Zain Khan Swati <zainkhanswati54@gmail.com>
2026-08-05 14:37:24 +0500

    Changes error

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,10 +3,14 @@ plugins {
     kotlin("jvm")
 }
 
-sourceCompatibility = JavaVersion.VERSION_17
-targetCompatibility = JavaVersion.VERSION_17
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
-val gdxVersion: String by rootProject.extra.properties.withDefault { "1.12.1" }
+kotlin {
+    jvmToolchain(17)
+}
 
 dependencies {
     val gdx = project.findProperty("gdxVersion") as String? ?: "1.12.1"
